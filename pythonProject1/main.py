1c3733113ad66f86f3cd10bd4d821c5881e02360
MessiWangSong <csw040505@qq.com>
2026-03-12 17:06:23 +0800

    测试

---
+++ pythonProject1/main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+def duozhi(num,*args,**kwargs):
+    print(num)
+    print(kwargs)
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+i = 100
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("hello world")
+num = 1
+print(f'{num}')
+print("我是谁")
+print(num,1)# change   list[] 赋值
+# not change
+duozhi(1,name="1")
